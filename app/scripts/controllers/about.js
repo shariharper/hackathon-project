9a6c56b17a42d2f7023ac10c5709c07e7682b198
Shari Harper <shari.harper@synapse-wireless.com>
2015-09-24 13:27:55 -0500

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hackathonProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hackathonProjectApp
+ */
+angular.module('hackathonProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hackathonProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hackathonProjectApp
+ */
+angular.module('hackathonProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
